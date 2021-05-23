e4539b24ebe426c4439bdc1d2214cf6f81b74a0e
rodrigoquezadab <rodrigo.quezadab@gmail.com>
2021-05-22 21:10:47 -0400

    primer comit del juego funcional

---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+/* Evento click de inicio de juego */
+
+document.getElementById("myBtnJugar").addEventListener("click", function () {
+  let puntajeMaquina = 0;
+  let puntajeJugador = 0;
+  let cantJuegos = +prompt("cuántos juegos quieres jugar?");
+
+  for (let i = 1; i <= cantJuegos; i++) {
+    let jugadaMaquina = 1 + Math.floor(Math.random() * 3);
+    let jugadaJugador = +prompt("Ingresa el numero de tu juego 1-✊ 2-📜 3-✂");
+    let jugadaGanador = "";
+    //Evaluacion de jugasas y resultados
+    if (jugadaMaquina == jugadaJugador) {
+      //empate
+      jugadaGanador = "Empate";
+    } else if (
+      (jugadaMaquina == 1 && jugadaJugador == 3) ||
+      (jugadaMaquina == 2 && jugadaJugador == 1) ||
+      (jugadaMaquina == 3 && jugadaJugador == 2)
+    ) {
+      jugadaGanador = "La Máquina Gana";
+      puntajeMaquina += 1;
+    } else {
+      jugadaGanador = "El Humano Gana";
+      puntajeJugador += 1;
+    }
+
+    //Verificaccion de datos de cada uego individual por consola
+    console.log(
+      `La Maquina jugo ${jugadaMaquina} y tu jugaste ${jugadaJugador} el gnador es ${jugadaGanador}`
+    );
+  }
+
+  //Mensaje final con Resultados de todos los juegos.
+  if (puntajeMaquina == puntajeJugador) {
+    document.querySelector(
+      ".juego"
+    ).innerHTML = `El juego ha terminado en empate, la máquina obtuvo ${puntajeMaquina} puntos y tu ${puntajeJugador} puntos.`;
+  } else if (puntajeJugador > puntajeMaquina) {
+    document.querySelector(
+      ".juego"
+    ).innerHTML = `Felicidades 🎉 ganaste el juego! La máquina obtuvo ${puntajeMaquina} puntos y tu ${puntajeJugador} puntos.`;
+  } else {
+    document.querySelector(
+      ".juego"
+    ).innerHTML = `Perdiste! La máquina obtuvo ${puntajeMaquina} puntos y tu ${puntajeJugador} puntos.`;
+  }
+});
